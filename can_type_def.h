ca6559e6a1ca1439d03e054235a1285c48b5fef6
Kunwoo Kim <starryeev@gmail.com>
2026-05-31 01:04:52 +0900

    fix: 데이터 최대로 보내도록 수정

--- can_type_def.h
+++ can_type_def.h
@@ -236,8 +236,8 @@ enum {
 
 /* UDS OTA 설정 */
 #define UDS_CANFD_MAX_PAYLOAD_SIZE     64U
-#define UDS_TRANSFER_DATA_SIZE         32U
-#define UDS_MAX_BLOCK_LENGTH           32U
+#define UDS_TRANSFER_DATA_SIZE         62U
+#define UDS_MAX_BLOCK_LENGTH           64U
 
 /*
  * RequestDownload simplified format
@@ -332,7 +332,7 @@ typedef struct CAN_PACKED {
  *
  * P0      : RSID = 0x74
  * P1      : LengthFormatIdentifier style value
- * P2~P3   : MaxNumberOfBlockLength = 32
+ * P2~P3   : MaxNumberOfBlockLength = 64
  */
 typedef struct CAN_PACKED {
     UdsServiceId_t responseSid;
