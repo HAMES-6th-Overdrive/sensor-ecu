56469291a38e39e1ac0eb65ce4e84cdc8600d411
Kunwoo Kim <starryeev@gmail.com>
2026-05-31 09:51:29 +0900

    fix: 페이로드 최대로 수정

--- UdsOta.h
+++ UdsOta.h
@@ -35,11 +35,15 @@
 #endif
 
 #ifndef UDS_TRANSFER_DATA_SIZE
-#define UDS_TRANSFER_DATA_SIZE        32U
+#define UDS_TRANSFER_DATA_SIZE        62U
 #endif
 
 #ifndef UDS_MAX_BLOCK_LENGTH
-#define UDS_MAX_BLOCK_LENGTH          32U
+#define UDS_MAX_BLOCK_LENGTH          64U
+#endif
+
+#if ((UDS_TRANSFER_DATA_SIZE + 2U) > UDS_CANFD_MAX_PAYLOAD_SIZE)
+#error "UDS_TRANSFER_DATA_SIZE exceeds CAN FD payload after SID/BSC overhead"
 #endif
 
 #ifndef UDS_APP_START_ADDR
@@ -54,7 +58,7 @@
  * 현재 구현 기준:
  * - CAN FD Raw Frame 사용
  * - 1 frame payload 최대 64 byte
- * - TransferData 실제 firmware data는 32 byte 단위
+ * - TransferData 실제 firmware data는 CAN FD 최대 62 byte 단위
  * - 추후 ISO-TP/CAN TP 확장 가능
  */
 #define UDS_OTA_CANFD_PAYLOAD_SIZE    UDS_CANFD_MAX_PAYLOAD_SIZE
